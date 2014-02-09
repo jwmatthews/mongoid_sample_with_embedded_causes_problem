2c8ea266ac3ed36889b84620ac1448370301b644
John Matthews <jwmatthews@gmail.com>
2014-02-09 09:22:21 -0500

    Initial setup of rails/mongoid/rspec

---
+++ mongoid_sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidSample::Application.config.secret_token = '000647e1b1fbb00f12acb17f35cb3df5574bdef1c838727a613ae705654d2e493b3270ab5a547bdfbfecb8fec4a32efa3a8e13a41670aa66224d8e77a77b30d2'
